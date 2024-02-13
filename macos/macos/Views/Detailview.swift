20fe810ae6d9784ed65887cd035d9cc737440159
H.Kobayashi <53347459+KobaJapan@users.noreply.github.com>
2024-02-14 06:00:06 +0900

    サイドバーの構築。Detailはこれから詰めます。

---
+++ macos/macos/Views/Detailview.swift
@@ -0,0 +1,18 @@
+//
+//  Detailview.swift
+//  macos
+//
+//  Created by 小林駿斗 on 2024/02/14.
+//
+
+import SwiftUI
+
+struct Detailview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Detailview()
+}
---
+++ macos/macos/Views/Sidebarview.swift
@@ -0,0 +1,23 @@
+//
+//  Sidebarview.swift
+//  macos
+//
+//  Created by 小林駿斗 on 2024/02/14.
+//
+
+import SwiftUI
+
+struct Sidebarview: View {
+    var body: some View {
+        List{
+            Text("ホーム")
+            Text("録画")
+            Text("アカウント")
+            
+        }
+    }
+}
+
+#Preview {
+    Sidebarview()
+}
